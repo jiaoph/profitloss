7f2e8c6a1841bfc97b8d94d674c444e4a62a3b91
sRect <fcqun@sina.cn>
2017-09-12 16:01:51 +0800

    rename components

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="contentWrap" class="content">
+    <Crumbs></Crumbs>
+  </div>
+</template>
+
+<script>
+import Crumbs from './Crumbs.vue'
+
+export default {
+  name: 'contentWrap',
+  components: {
+    Crumbs
+  }
+}
+</script>
+
+
+<style lang='less'>
+@import url('../assets/less/public');
+.content{
+    width: 1200px;
+    margin: 0 auto;
+    padding: 0 20px;
+    background-color: #fff;
+    .boxSizing();
+}
+</style>
+

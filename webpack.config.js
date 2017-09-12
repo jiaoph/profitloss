7bd74afd7d089e93190b2c0c99165cf2088eeff3
sRect <fcqun@sina.cn>
2017-09-12 11:27:37 +0800

    修改配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,18 @@ module.exports = {
         exclude: /node_modules/
       },
       {
+        test: /\.less$/,
+        loader: 'less-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+        loader: 'file-loader'
+      },
+      {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {

3a3fa22844df07551eba6b72b817d5cb993849e9
sRect <fcqun@sina.cn>
2017-09-13 09:57:48 +0800

    add Crumbs.vue

---
+++ src/components/Crumbs.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="crumbs" class="crumbs">
+        <el-breadcrumb class="breadcrumb" separator=">">
+            <el-breadcrumb-item :to="{ path: '/' }">轩天实业</el-breadcrumb-item>
+            <el-breadcrumb-item>财务中心</el-breadcrumb-item>
+        </el-breadcrumb>
+    </div>
+</template>
+
+<style scoped lang="less">
+.crumbs {
+    height: 54px;
+    width: 100%;
+    border-bottom: 1px solid #ddd;
+    >.breadcrumb{
+        line-height: 60px;
+    }
+}
+</style>
+

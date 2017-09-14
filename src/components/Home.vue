f4c06ffd5537432c019d742f856fd630da850b0a
sRect <fcqun@sina.cn>
2017-09-14 18:07:10 +0800

    修改样式

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,12 +9,16 @@
         <BarChart></BarChart>
       </div>
     </div>
-    <div class="OpeIncome clearfix">
+    <div class="OpeIncome">
       <h2 class="subtitle">主营业收入</h2>
-      <div class="pie fl">
-        <PieChart></PieChart>
+      <div class="content clearfix">
+        <div class="pie fl">
+          <PieChart></PieChart>
+        </div>
+        <div class="tableIncome fl">
+          <TableIncome></TableIncome>
+        </div>
       </div>
-      <div class="tableIncome fl"></div>
     </div>
   </div>
 </template>
@@ -24,6 +28,7 @@ import Crumbs from './Crumbs.vue'
 import Tabs from './Tabs.vue'
 import BarChart from './BarChart.vue'
 import PieChart from './PieChart.vue'
+import TableIncome from './TableIncome.vue'
 
 export default {
   name: 'contentWrap',
@@ -31,7 +36,8 @@ export default {
     Crumbs,
     Tabs,
     BarChart,
-    PieChart
+    PieChart,
+    TableIncome
   }
 }
 </script>
@@ -39,36 +45,41 @@ export default {
 
 <style lang='less'>
 @import url('../assets/less/public');
-.content{
-    width: 1200px;
-    margin: 0 auto;
-    padding: 0 20px;
-    background-color: #fff;
+.content {
+  width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px;
+  background-color: #fff;
+  .boxSizing();
+  >.mainBusiness {
+    width: 100%;
+    padding-top: 16px;
+    >.tab {
+      width: 416px;
+      padding-left: 40px;
+    }
+    >.barChart {
+      width: 510px;
+      padding-right: 100px;
+    }
+  }
+  >.OpeIncome {
+    width: 100%;
+    padding: 40px 0 0 40px;
     .boxSizing();
-    >.mainBusiness{
+    >.subtitle {
+      font-size: 20px;
+      font-weight: normal;
+    }
+    >.content {
+      padding-top: 20px;
       width: 100%;
-      padding-top: 16px;
-      >.tab{
-        width: 416px;
-        padding-left: 40px;
-      }
-      >.barChart{
-        width: 510px;
-        padding-right: 100px;
+      >.pie {
+        padding-top: 40px;
+        margin-right:46px;
       }
     }
-    >.OpeIncome{
-      width: 100%;
-      padding:40px 0 0 40px;
-      >.subtitle{
-        font-size: 20px;
-        font-weight: normal;
-      }
-      >.pie{
-        margin-left: 40px;
-        padding: 20px;
-      }
-    }
+  }
 }
 </style>
 

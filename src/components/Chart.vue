b5aaf6eea46d647f3272e118a449974312a4560b
sRect <fcqun@sina.cn>
2017-09-14 10:50:22 +0800

    修改堆叠图显示，未完成

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -24,20 +24,44 @@ export default {
                 }
             },
             legend: {
-                data: ['主营业收入', '主营业成本']
+                data: ['主营业收入', '主营业成本', '', '', '', ''],
+                x: "center",
+                y: 'bottom'
             },
             grid: {
+                top: '3%',
                 left: '3%',
                 right: '4%',
-                bottom: '3%',
+                // bottom: '3%',
                 containLabel: true
             },
             xAxis: {
                 type: 'category',
-                data:['主营业收入', '主营业成本']
+                axisLabel: {
+                    show: true,
+                    textStyle: {
+                        color: '#333',
+                        fontSize: '14'
+                    }
+                },
+                splitLine: {
+                    // show: false,
+                    //  改变轴线颜色
+                    lineStyle: {
+                        // 使用深浅的间隔色
+                        color: '#d6d6d6',
+                    }
+                },
+                axisLine: {
+                    lineStyle: {
+                        color: '#fff',
+                        width: 3,   //这里是坐标轴的宽度,可以去掉
+                    }
+                },
+                data: ['主营业收入', '主营业成本', ' ', ' ', ' ', ' ']
             },
             yAxis: {
-                type: 'value'
+                type: 'value',
             },
             series: [
                 {
@@ -50,7 +74,7 @@ export default {
                             position: 'insideRight'
                         }
                     },
-                    data: [320, 3024]
+                    data: [320, 304, 0, 0, 0, 0]
                 },
                 {
                     name: '主营业成本',
@@ -62,7 +86,55 @@ export default {
                             position: 'insideRight'
                         }
                     },
-                    data: [120, 132]
+                    data: [240, 123, 0, 0, 0, 0]
+                },
+                {
+                    name: '3',
+                    type: 'bar',
+                    stack: '总量',
+                    label: {
+                        normal: {
+                            show: true,
+                            position: 'insideRight'
+                        }
+                    },
+                    data: [0, 234, 0, 0, 0, 0]
+                },
+                {
+                    name: '4',
+                    type: 'bar',
+                    stack: '总量',
+                    label: {
+                        normal: {
+                            show: true,
+                            position: 'insideRight'
+                        }
+                    },
+                    data: [0, 456, 0, 0, 0, 0]
+                },
+                {
+                    name: '5',
+                    type: 'bar',
+                    stack: '总量',
+                    label: {
+                        normal: {
+                            show: true,
+                            position: 'insideRight'
+                        }
+                    },
+                    data: [0, 0, 0, 0, 0, 0]
+                },
+                {
+                    name: '6',
+                    type: 'bar',
+                    stack: '总量',
+                    label: {
+                        normal: {
+                            show: true,
+                            position: 'insideRight'
+                        }
+                    },
+                    data: [0, 0, 0, 0, 0, 0]
                 }
             ]
         });
@@ -73,7 +145,7 @@ export default {
 <style scoped>
 .barWrap {
     width: 100%;
-    height: 500px;
+    height: 420px;
 }
 </style>
 

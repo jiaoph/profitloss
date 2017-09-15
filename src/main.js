9e9bd9099899bfb80963dc2da284a8391c6929a3
sRect <fcqun@sina.cn>
2017-09-15 18:12:18 +0800

    引入element-ui Table组件

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
-import { Breadcrumb, BreadcrumbItem } from 'element-ui'
+import { Breadcrumb, BreadcrumbItem,Table,TableColumn } from 'element-ui'
 import App from './App.vue'
 
 Vue.component(Breadcrumb.name, Breadcrumb)
-Vue.component(BreadcrumbItem.name, BreadcrumbItem)  
+Vue.component(BreadcrumbItem.name, BreadcrumbItem)
+Vue.component(Table.name, Table)
+Vue.component(TableColumn.name, TableColumn)
 
 new Vue({
   el: '#app',

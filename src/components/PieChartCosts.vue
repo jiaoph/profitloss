4b52a99ccd3d4a60859d80cb415e9419f0500588
sRect <fcqun@sina.cn>
2017-09-15 13:59:22 +0800

    PieChartCosts.vue 初步完成

--- src/components/PieChartCosts.vue
+++ src/components/PieChartCosts.vue
@@ -18,15 +18,15 @@ export default {
         trigger: 'item',
         formatter: "{a} <br/>{b}: {c} ({d}%)"
       },
-      color:['#97C681','#CAD879','#FBB661','#DEE598','#4A9973'],
+      color:['#9ECCE8','#489ED3','#6AB7CC','#32A5A2','#AAE5D9','#9DACD4','#5665C1'],
       legend: {
         orient: 'vertical',
         x: 'left',
-        data: ['溢价','佣金收入',  '已收团购', '应收团购','甲方奖励',]
+        data: ['案场费用','联动费用',  '渠道佣金', '开发费用','管理费用1','管理费用2','财务费用']
       },
       series: [
         {
-          name: '主营业收入',
+          name: '主营业务成本',
           type: 'pie',
           radius: ['50%', '90%'],
           // center: ['50%', '50%'],
@@ -51,11 +51,13 @@ export default {
             }
           },
           data: [
-            { value: 310, name: '溢价' },
-            { value: 335, name: '佣金收入' },
-            { value: 190, name: '已收团购' },
-            { value: 248, name: '应收团购' },
-            { value: 234, name: '甲方奖励' }
+            { value: 310, name: '案场费用' },
+            { value: 335, name: '联动费用' },
+            { value: 190, name: '渠道佣金' },
+            { value: 248, name: '开发费用' },
+            { value: 204, name: '管理费用1' },
+            { value: 114, name: '管理费用2' },
+            { value: 594, name: '财务费用' }
           ]
         }
       ]

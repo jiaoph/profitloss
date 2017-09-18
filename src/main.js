21aebd2355e46da5bbfe3befebd57f9dd6e9ff73
sRect <fcqun@sina.cn>
2017-09-18 14:20:50 +0800

    引入tooltip组件

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
-import { Breadcrumb, BreadcrumbItem,Table,TableColumn } from 'element-ui'
+import { Breadcrumb, BreadcrumbItem,Table,TableColumn,Tooltip } from 'element-ui'
 import App from './App.vue'
 
 Vue.component(Breadcrumb.name, Breadcrumb)
 Vue.component(BreadcrumbItem.name, BreadcrumbItem)
 Vue.component(Table.name, Table)
 Vue.component(TableColumn.name, TableColumn)
+Vue.component(Tooltip.name, Tooltip)
 
 new Vue({
   el: '#app',

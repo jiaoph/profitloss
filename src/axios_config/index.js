0ff95155aa46cb806c423ed33d7b10d43a937ae0
sRect <fcqun@sina.cn>
2017-09-18 17:51:17 +0800

    创建axios配置

---
+++ src/axios_config/index.js
@@ -0,0 +1,89 @@
+import axios from "axios";
+import qs from "qs";
+import { Message, Loading } from "element-ui";
+
+
+const Axios = axios.create({
+  baseURL: "http://192.168.1.32:8080",
+  // baseURL: "http://192.168.1.76",
+  // baseURL: "http://192.168.1.76",
+  timeout: 5000,
+  responseType: "json",
+  // withCredentials: true, // 是否允许带cookie这些
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+  }
+});
+
+let loadingInstance = null;
+
+//POST传参序列化(添加请求拦截器)
+Axios.interceptors.request.use(
+  config => {
+    // 在发送请求之前做某件事
+    if (
+      config.method === "post" ||
+      config.method === "put" ||
+      config.method === "delete"
+    ) {
+      // 序列化
+      config.data = qs.stringify(config.data);
+    }
+
+    // 若是有做鉴权token , 就给头部带上token
+    // if (localStorage.token) {
+    //   config.headers.Authorization = localStorage.token;
+    // }
+
+    loadingInstance = Loading.service({
+      text: '正在加载...'
+    });
+
+    return config;
+  },
+  error => {
+    console.log(error)
+    Message({
+      //  饿了么的消息弹窗组件,类似toast
+      showClose: true,
+      message: error,
+      type: "error.data.error.message"
+    });
+    loadingInstance.close()
+    return Promise.reject(error.data.error.message);
+  }
+);
+
+Axios.interceptors.response.use(
+  res => {
+    // console.log(res)
+    loadingInstance.close()
+    //对响应数据做些事
+    // if (res.data && !res.data.success) {
+    //   Message({
+    //     //  饿了么的消息弹窗组件,类似toast
+    //     showClose: true,
+    //     message: res.data.error.message.message
+    //       ? res.data.error.message.message
+    //       : res.data.error.message,
+    //     type: "error"
+    //   });
+    //   return Promise.reject(res.data.error.message);
+    // }
+    return res;
+  },
+  error => {
+    console.log(error)
+    loadingInstance.close()
+    // 返回 response 里的错误信息
+    let errorInfo =  error.data.error ? error.data.error.message : error.data;
+    return Promise.reject(errorInfo);
+  }
+);
+
+
+export default {
+  install: function (Vue, Option) {
+    Object.defineProperty(Vue.prototype, "$http", { value: Axios });
+  }
+};

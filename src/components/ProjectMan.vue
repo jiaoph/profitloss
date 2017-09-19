1311780335aca36abbb8059fcb5dd332a0616c47
sRect <fcqun@sina.cn>
2017-09-19 16:53:56 +0800

    ProjectMan.vue完成

--- src/components/ProjectMan.vue
+++ src/components/ProjectMan.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="project" class="projectTable">
-    <el-table :data="tableData2" border style="width: 100%" @expand="handle">
+    <el-table :data="tableData2" height="600" border style="width: 100%" @expand="handle" :row-key="getRowKeys" :expand-row-keys="expands">
       <el-table-column type="expand" width="30">
         <template scope="scope">
           <el-table :data="in_tableData2" border :show-header="false" style="width: 100%" class="table-expand">
-            <el-table-column prop="in_area" align="center" width="120" :show-overflow-tooltip="true"></el-table-column>
+            <el-table-column prop="area" align="center" width="120" :show-overflow-tooltip="true"></el-table-column>
             <el-table-column prop="in_totalIncome" align="center" :show-overflow-tooltip="true"></el-table-column>
-            <el-table-column prop="in_commisionIncome" align="center" :show-overflow-tooltip="true"></el-table-column>
-            <el-table-column prop="in_premium" align="center" :show-overflow-tooltip="true"></el-table-column>
-            <el-table-column prop="in_award" align="center" :show-overflow-tooltip="true"></el-table-column>
-            <el-table-column prop="in_receivedGroupon" align="center" :show-overflow-tooltip="true"></el-table-column>
-            <el-table-column prop="in_accountsGroupon" align="center" :show-overflow-tooltip="true"></el-table-column>
+            <el-table-column prop="commissionEarned" align="center" :show-overflow-tooltip="true"></el-table-column>
+            <el-table-column prop="premium" align="center" :show-overflow-tooltip="true"></el-table-column>
+            <el-table-column prop="partyReward" align="center" :show-overflow-tooltip="true"></el-table-column>
+            <el-table-column prop="receivedGroup" align="center" :show-overflow-tooltip="true"></el-table-column>
+            <el-table-column prop="receivableGroup" align="center" :show-overflow-tooltip="true"></el-table-column>
             <el-table-column prop="in_totalCost" align="center" :show-overflow-tooltip="true"></el-table-column>
-            <el-table-column prop="in_acExpense" align="center" :show-overflow-tooltip="true"></el-table-column>
-            <el-table-column prop="in_ldExpense" align="center" :show-overflow-tooltip="true"></el-table-column>
-            <el-table-column prop="in_channelCom" align="center" :show-overflow-tooltip="true"></el-table-column>
-            <el-table-column prop="in_apportion" align="center" :show-overflow-tooltip="true"></el-table-column>
+            <el-table-column prop="saleExpense" align="center" :show-overflow-tooltip="true"></el-table-column>
+            <el-table-column prop="placeExpense" align="center" :show-overflow-tooltip="true"></el-table-column>
+            <el-table-column prop="channelCommission" align="center" :show-overflow-tooltip="true"></el-table-column>
+            <el-table-column prop="shareExpense" align="center" :show-overflow-tooltip="true"></el-table-column>
             <el-table-column prop="in_allcount" align="center" :show-overflow-tooltip="true" class-name="special"></el-table-column>
           </el-table>
         </template>
@@ -55,68 +55,25 @@
 </template>
 
 <script>
+import { Message } from "element-ui";
 export default {
   data() {
     return {
-      tableData2: [
-        // {
-        //   area: '第一事业部第一事业部333',
-        //   totalIncome: 300000000,
-        //   commisionIncome: 2324234234234,
-        //   premium: 239,
-        //   award: 987,
-        //   receivedGroupon: 67,
-        //   accountsGroupon: 591,
-        //   totalCost: 670,
-        //   acExpense: 45,
-        //   ldExpense: 190,
-        //   channelCom: 98705,
-        //   apportion: 7554,
-        //   allcount: 12314
-        // }
-      ],
-      in_tableData2: [
-        {
-          in_area: '第一事业部第一事业部',
-          in_totalIncome: 300000000,
-          in_commisionIncome: 23,
-          in_premium: 239,
-          in_award: 987,
-          in_receivedGroupon: 67,
-          in_accountsGroupon: 591,
-          in_totalCost: 670,
-          in_acExpense: 45,
-          in_ldExpense: 190,
-          in_channelCom: 98705,
-          in_apportion: 7554,
-          in_allcount: 123090149999
-        },
-        {
-          in_area: '第一事业部第一事业部',
-          in_totalIncome: 300000000,
-          in_commisionIncome: 23,
-          in_premium: 239,
-          in_award: 987,
-          in_receivedGroupon: 67,
-          in_accountsGroupon: 591,
-          in_totalCost: 670,
-          in_acExpense: 45,
-          in_ldExpense: 190,
-          in_channelCom: 98705,
-          in_apportion: 7554,
-          in_allcount: 12314
-        }
-      ]
+      tableData2: [],
+      in_tableData2: [],
+      lastarea: '',
+      lastexpanded: '',
+      expands: []
     }
   },
   methods: {
-    getData() {
+    getData(xtype, xname, xtime) {
       this.$http.post('/efangfin/outgoing/table.do', {
-        xtype: 0,
-        xname: 0,
-        xtime: 0
+        xtype: xtype,
+        xname: xname,
+        xtime: xtime
       }).then(data => {
-        console.log(data)
+        // console.log(data)
         let myData = data.data;
         if (JSON.stringify(myData)) {
           let status = myData.status;
@@ -124,32 +81,56 @@ export default {
             case 1:
               let inData = myData.data;
               if (inData.length) {
-                this.tableData2 = inData;
+                switch (xtype) {
+                  case "0":
+                    this.tableData2 = inData;
+                    break;
+                  case "1":
+                    this.in_tableData2 = inData;
+                    break;
+                  case "2":
+                    break;
+                  default:
+                    break;
+                }
+
               } else {
-                this.$message({
+                switch (xtype) {
+                  case "0":
+                    this.tableData2 = [];
+                    break;
+                  case "1":
+                    this.in_tableData2 = [];
+                    break;
+                  case "2":
+                    break;
+                  default:
+                    break;
+                }
+                Message({
                   showClose: true,
-                  message: '项目经营情况数据为空',
+                  message: '数据为空',
                   type: 'warning'
                 });
               }
               break;
             case 0:
-              this.$message({
+              Message({
                 showClose: true,
                 message: '获取项目经营情况错误',
                 type: 'error'
               });
               break;
             default:
-              this.$message({
+              Message({
                 showClose: true,
-                message: '获取项目经营情况异常status',
+                message: '项目经营情况异常status',
                 type: 'error'
               });
               break;
           }
         } else {
-          this.$message({
+          Message({
             showClose: true,
             message: '暂无数据',
             type: 'warning'
@@ -157,15 +138,52 @@ export default {
           return;
         }
       }).catch(error => {
+        Message({
+          showClose: true,
+          message: '项目经营情况异常错误',
+          type: 'error'
+        });
         console.log(error)
       })
     },
     handle(row, expanded) {
-      console.log(row, expanded)
+      let type = row.type,
+        area = row.area;
+
+      if (area !== this.lastarea) {
+        // console.log('点击的不是同一个');
+        if (this.lastexpanded) {
+          // console.log('之前的应该关闭');
+          this.expands = [];
+        }
+      }
+
+      if (expanded) {
+        switch (type) {
+          case "2":
+            break;
+          case "1":
+            this.lastarea = area;
+            this.lastexpanded = expanded;
+
+            this.getData(type, area, "0");
+            this.expands.push(area)
+            break;
+          case "0":
+            break;
+        }
+      } else {
+        this.expands = [];
+        this.lastexpanded = expanded;
+      };
     },
+    getRowKeys(row) {
+      return row.area;
+    }
   },
   mounted() {
-    this.getData();
+    this.getData("0", "0", "0"); //营运中心默认获取
+
   }
 }
 </script>

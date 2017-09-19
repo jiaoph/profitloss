83bed711b995d5cf642ea24162a0d132b8f2fd4f
sRect <fcqun@sina.cn>
2017-09-19 11:23:19 +0800

    引入过滤器

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
-import { Breadcrumb, BreadcrumbItem,Table,TableColumn,Tooltip } from 'element-ui'
+import { Breadcrumb, BreadcrumbItem,Table,TableColumn,Tooltip,Message } from 'element-ui'
 import axiosPlugin from './axios_config/index'
 import App from './App.vue'
+import filters from './filters/'
 
 Vue.use(axiosPlugin);
+Object.keys(filters).forEach(key => Vue.filter(key, filters[key]))
 
 Vue.component(Breadcrumb.name, Breadcrumb)
 Vue.component(BreadcrumbItem.name, BreadcrumbItem)
 Vue.component(Table.name, Table)
 Vue.component(TableColumn.name, TableColumn)
 Vue.component(Tooltip.name, Tooltip)
+Vue.component(Message.name, Message)
 
 new Vue({
   el: '#app',

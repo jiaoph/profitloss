1c337ecc9473ad2629bf22b911981330a6e5f810
sRect <fcqun@sina.cn>
2017-09-25 13:24:03 +0800

    修改接口url

--- src/axios_config/index.js
+++ src/axios_config/index.js
@@ -4,7 +4,7 @@ import { Message, Loading } from "element-ui";
 
 
 const Axios = axios.create({
-  baseURL: "http://192.168.1.13:8080",
+  baseURL: "http://192.168.1.15:8080",
   // baseURL: "http://192.168.1.76",
   // baseURL: "http://192.168.1.76",
   timeout: 5000,

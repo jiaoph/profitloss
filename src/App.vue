e223ecf45c2663505f7ff41b9bbdbab97a92e025
sRect <fcqun@sina.cn>
2017-09-26 14:32:02 +0800

    增加全局样式

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,12 @@ import HeaderVue from './components/HeaderVue.vue'
 
 export default {
   name: 'app',
-  data () {
+  data() {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
   },
-  components:{
+  components: {
     HeaderVue,
     // Home
   }
@@ -29,4 +29,32 @@ export default {
 <style rel="stylesheet/less" lang="less">
 @import "./assets/less/reset.css";
 @import "./assets/less/reset_elementui.less";
+@import "./assets/less/public";
+
+.content {
+  width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px 40px 20px;
+  background-color: #fff;
+  .boxSizing();
+}
+
+.timeselect {
+  width: 76px;
+  height: 24px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  >.resetSelect {
+    .el-input__icon {
+      color: #EC9028;
+    }
+    .el-input__inner {
+      color: #ED9127;
+      font-size: 12px;
+      background-color: #FCF1E5;
+      border-color: #ED9127;
+    }
+  }
+}
 </style>

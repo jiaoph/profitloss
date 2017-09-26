eaa758ee5becf7b7b71b35439912384298b90a96
sRect <fcqun@sina.cn>
2017-09-26 14:34:46 +0800

    初步完成SupportDepartCosts.vue

--- src/components/SupportDepartCosts.vue
+++ src/components/SupportDepartCosts.vue
@@ -1,20 +1,28 @@
 <template>
-  <div id="anchang">
+  <div id="anchang" class="content">
+    <Crumbs></Crumbs>
+    <div class="clearfix"></div>
+    <div class="title clearfix">
+      <h3 class="fl">
+        <span>支持部门成本</span>
+        <span class="totalCount">
+          <b>202088883</b>元</span>
+      </h3>
+      <div class="timeselect fr">
+        <el-select v-model="value" @change="change" placeholder="请选择" class="resetSelect">
+          <el-option v-for="item in options" 
+            :key="item.value" 
+            :label="item.label" 
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </div>
+    </div>
     <div class="wrapper clearfix">
-      <ul class="lie" 
-        v-for="(val,index) in initjson" 
-        :data-index="val.index" 
-        :key="index"
-        :class="{expandWrap:val.index !== '0'}">
+      <ul class="lie" v-for="(val,index) in initjson" :data-index="val.index" :key="index" :class="{expandWrap:val.index !== '0'}">
         <li class="thead" :title="val.name">
           <span>{{val.name}}</span>
-          <span v-show="val.expand" 
-            @click.stop.prevent="handleExpand($event)" 
-            :data-status="0" 
-            :data-order="index" 
-            :data-index='Number.parseInt(val.index)+1'
-            title="点击展开/收缩" 
-            class="throw-right">
+          <span v-show="val.expand" @click.stop.prevent="handleExpand($event)" :data-status="0" :data-order="index" :data-index='Number.parseInt(val.index)+1' title="点击展开/收缩" class="throw-right">
           </span>
         </li>
         <li :title="val.zhaomu">
@@ -41,12 +49,35 @@
 </template>
 
 <script>
+import Crumbs from './Crumbs'
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'anchang',
+  computed: mapGetters([
+    'time_val'
+  ]),
+  components: {
+    Crumbs
+  },
   data() {
     return {
+      options: [{
+        value: '0',
+        label: '全部'
+      }, {
+        value: '1',
+        label: '上月'
+      }, {
+        value: '2',
+        label: '本月'
+      }, {
+        value: '3',
+        label: '本年'
+      }],
+      value: '0', //默认时间维度
       flag: true, // 测试flag
-      deleteIndexArr:[],
+      deleteIndexArr: [],
       initjson: [
         {
           name: '部门',
@@ -126,14 +157,37 @@ export default {
     }
   },
   methods: {
-    removeClass(el,className){
+    change(val){
+      this.timeval = val;
+      this.$store.dispatch('timechange', val);
+    },
+    getData(xtype,xname,xtime,xnumber,index){
+      this.$http.post('/efangfin/outgoing/twoLevelCost.do',{
+        xtype: xtype,
+        xname: xname,
+        xtime: xtime,
+        xnumber: xnumber,
+        index: index
+      }).then(data => {
+        console.log(data);
+      }).catch(error => {
+        Message({
+          showClose: true,
+          duration: 1800,
+          message: '支持部门获取数据异常',
+          type: 'error'
+        });
+        console.log(error);
+      })
+    },
+    removeClass(el, className) {
       el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
     },
     handleExpand(e) {
       let status = e.currentTarget.getAttribute('data-status'), // 关闭展开状态
-        order = Number.parseInt(e.currentTarget.getAttribute('data-order'))+1, // 点击时获取到的数组的order
+        order = Number.parseInt(e.currentTarget.getAttribute('data-order')) + 1, // 点击时获取到的数组的order
         out_index = ''; // 点击时获取到的层级
-        
+
       switch (status) {
         case '0': // 该展开添加数据
           e.currentTarget.setAttribute('data-status', '1');
@@ -143,16 +197,16 @@ export default {
           e.currentTarget.parentNode.parentNode.className += ' ' + 'specialborder'; // 添加class
           // console.log('out_index-->'+out_index)
           // 发送请求加载数据
-          if(this.flag){
+          if (this.flag) {
             this.flag = false;
-            if(this.regularjson1.length){
-              for(let i = 0,len = this.regularjson1.length; i < len; i++){
+            if (this.regularjson1.length) {
+              for (let i = 0, len = this.regularjson1.length; i < len; i++) {
                 this.initjson.splice(order++, 0, this.regularjson1[i]);
               }
             }
-          }else{
-            if(this.regularjson2.length){
-              for(let i = 0,len = this.regularjson2.length; i < len; i++){
+          } else {
+            if (this.regularjson2.length) {
+              for (let i = 0, len = this.regularjson2.length; i < len; i++) {
                 this.initjson.splice(order++, 0, this.regularjson2[i]);
               }
             }
@@ -164,19 +218,19 @@ export default {
           let inner_index = Number.parseInt(e.currentTarget.getAttribute('data-index'));
           // console.log('inner_index-->'+inner_index)
           if (inner_index === out_index || inner_index > out_index) { // 关闭时的层级和外层的层级对比
-            this.initjson.forEach((val,i,arr) => {
+            this.initjson.forEach((val, i, arr) => {
               let new_index = Number.parseInt(val.index);
 
-              if(inner_index === new_index || inner_index < new_index){ // 关闭的层级和initjson数组元素的层级对比
+              if (inner_index === new_index || inner_index < new_index) { // 关闭的层级和initjson数组元素的层级对比
                 this.deleteIndexArr.push(i);
               }
             })
 
             let newArr = Array.prototype.slice.call(this.deleteIndexArr),
               // maxNum = Math.max.apply(null,newArr),
-              minNum = Math.min.apply(null,newArr); // 数组最小值
+              minNum = Math.min.apply(null, newArr); // 数组最小值
 
-            this.initjson.splice(minNum,newArr.length); // 初始数组删除元素
+            this.initjson.splice(minNum, newArr.length); // 初始数组删除元素
             this.deleteIndexArr = []; // 置空deleteIndexArr
             this.removeClass(e.currentTarget, 'throw-left');
             e.currentTarget.className += ' ' + 'throw-right';
@@ -187,14 +241,43 @@ export default {
           break;
       }
     }
+  },
+  mounted(){
+    this.value = this.time_val;
   }
 }
 </script>
 
 <style scoped>
+.title {
+  line-height: 70px;
+  position: relative;
+}
+
+.title h3 {
+  font-size: 20px;
+  font-weight: normal;
+}
+
+.title h3 .totalCount {
+  font-family: 'Helvetica';
+  color: #EF9128;
+  margin-left: 20px;
+}
+
+.title h3 .totalCount b {
+  font-weight: normal;
+}
+
 .wrapper {
   width: 100%;
   font-size: 12px;
+  white-space: nowrap;
+  overflow: hidden;
+  overflow-x: scroll;
+  -webkit-backface-visibility: hidden;
+  -webkit-perspective: 1000;
+  -webkit-overflow-scrolling: touch;
 }
 
 .wrapper ul {
@@ -202,11 +285,11 @@ export default {
   border-right: 1px solid #ddd;
 }
 
-.wrapper ul.specialborder{
-  border-right-color:#F5C38B;
+.wrapper ul.specialborder {
+  border-right-color: #F5C38B;
 }
 
-.wrapper ul:first-child{
+.wrapper ul:first-child {
   border-left: 1px solid #ddd;
 }
 
@@ -238,7 +321,7 @@ export default {
   position: relative;
 }
 
-.expandWrap li.thead{
+.expandWrap li.thead {
   border-top: 1px solid #F5C38B;
   background-color: #FCF1E5;
 }
@@ -249,7 +332,7 @@ export default {
   background-color: #f8f8f8;
 }
 
-.expandWrap li.foot{
+.expandWrap li.foot {
   border-bottom: 1px solid #F5C38B;
   background-color: #FCF1E5;
 }

ef88d55cc6f64067e8fbcd036db88e469a5f9eb6
sRect <fcqun@sina.cn>
2017-09-28 14:44:58 +0800

    修改moneyFormat过滤器

--- src/filters/moneyFormat.js
+++ src/filters/moneyFormat.js
@@ -1,3 +1,7 @@
 export const formateMoney = (data) => {
-  console.log(data)
+  if (typeof data === 'number') {
+    return data.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  } else {
+    return data;
+  }
 }

6f4ec44a5bfc80abc65c401abe96199f2b135db0
sRect <fcqun@sina.cn>
2017-09-28 14:43:42 +0800

    修改getCount函数

--- src/filters/getCount.js
+++ src/filters/getCount.js
@@ -1,4 +1,13 @@
-export const getCount = (data) => {
-  console.log(data)
-  return '99'
+export const getCount = (obj) => {
+  let total = obj.type === 0 ? 0 : -1;
+  
+  Object.keys(obj).forEach(key => {
+    if(typeof obj[key] === 'number') {
+      total += obj[key];
+    }else{
+      total += 0;
+    }
+  })
+
+  return total === -1 ? '总计' : Number.parseFloat(total.toFixed(2));
 }

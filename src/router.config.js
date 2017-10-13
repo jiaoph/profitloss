01536c4256382204ff28091f384e270890b790b3
sRect <fcqun@sina.cn>
2017-10-13 16:30:29 +0800

    添加子路由

--- src/router.config.js
+++ src/router.config.js
@@ -1,11 +1,22 @@
 import Home from './components/Home'
 import SupportDepartCosts from './components/SupportDepartCosts'
+import TabsOpeProfit from './components/TabsOpeProfit'
 import Error from './components/Error'
 
 export default [
   {
     path: '/home',
-    component: Home
+    component: Home,
+    children: [
+      {
+        path: '/',
+        component: TabsOpeProfit
+      },
+      {
+        path: 'tabsOpeProfit',
+        component: TabsOpeProfit
+      }
+    ]
   },
   {
     path: '/supportDepartCosts',

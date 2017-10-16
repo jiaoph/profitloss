bf6a5a724ccaac680f1b212fafd235d1cf21b715
sRect <fcqun@sina.cn>
2017-10-16 14:45:22 +0800

    将vuex数据存入sessionStorage，解决浏览器刷新数据丢失

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,8 +1,18 @@
 export default {
   time_val(state) {
-    return state.timeval;
+    let timeval = sessionStorage.getItem('timeval');
+    if (timeval) {
+      return timeval;
+    } else {
+      return state.timeval;
+    }
   },
   fzixnumber_id(state) {
-    return state.fzixnumber;
+    let fzixnumber = sessionStorage.getItem('fzixnumber');
+    if (fzixnumber) {
+      return fzixnumber;
+    } else {
+      return state.fzixnumber;
+    }
   }
 }

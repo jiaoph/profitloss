a2eac139150dda7bbd6290ceb41289f1a1602dc8
sRect <fcqun@sina.cn>
2017-10-25 17:48:18 +0800

    修复fzixnumber从sessionStorage中取出来重复bug

--- src/store/getters.js
+++ src/store/getters.js
@@ -3,6 +3,7 @@ export default {
      return state.timeval;
   },
   fzixnumber_id(state) {
+    sessionStorage.clear();
     let fzixnumber = sessionStorage.getItem('fzixnumber');
     if (fzixnumber) {
       return fzixnumber;
